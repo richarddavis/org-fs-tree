84fa7e96e3d761187cbcf9bbc28cf018029b6fd1
Richard Davis <richard.lee.davis@gmail.com>
2022-12-15 12:38:33 +0100

    Numerous changes to the tree output
    1. Only .org files and directories are included
    2. Use #+title value in file for link title if it exists
    3. Use org ID from the top of the file if it exists and use this to
    create a link

--- org-fs-tree.el
+++ org-fs-tree.el
@@ -33,32 +33,60 @@
 (define-namespace org-fs-tree-
 
 (defun -make-link (s link)
-  (concat "[[" link "][" s "]]"))
+  (if (f-dir? link)
+      (concat "[[" link "][" s "]]")
+    (concat "[[" (-get-orgroam-id link) "][" (-get-orgroam-title link) "]]")))
+
+;; Pull org ID from the top of the file if it exists and use this to create a link
+(defun -get-orgroam-id (link)
+  (if
+      (equal
+       (nth 1 (split-string (f-read-text link)))
+       ":ID:")
+      (concat "id:" (nth 2 (split-string (f-read-text link))))
+    link
+    ))
+
+;; Use #+title value in file for link title if it exists
+(defun -get-orgroam-title (link)
+  (with-temp-buffer
+    (insert-file-contents link)
+    (goto-char (point-min))
+    (car (cdar (org-collect-keywords '("title"))))))
 
 (defun -make-heading (s level)
   (concat (make-string level ?*) " " s "\n"))
 
+;; Added so only .org files and directories are included
+(defun -include-file (file)
+  (and
+   ;; (not (f-hidden? file))
+   (or (f-ext? file "org") (f-dir? file)) ; Include org files or directories
+   (not (string-suffix-p "~" file)) ; Ignore backup files
+   (f-exists? file))) ; Not sure this does anything
+
 (defun -create-tree (base-dir level limit-level)
-  (let* ((full-path (f-full base-dir))
-	 (short-name  (if (f-dir? base-dir)
-			  (concat (f-filename base-dir) "/")
-			(f-filename base-dir)))
-	 (link (-make-link short-name full-path))
-	 (heading (-make-heading link level)))
-    (if (or (null limit-level) (< level limit-level))
-	(if (f-directory? base-dir)
-	    (concat heading
-		    (apply 'concat (mapcar
-				    (lambda (d)  (-create-tree d (+ 1 level) limit-level))
-				    (f-entries base-dir))))
-	  heading)
-      heading)))
+  (if (-include-file base-dir)
+      (let* ((full-path (f-full base-dir))
+	     (short-name  (if (f-dir? base-dir)
+			      (concat (f-filename base-dir) "/")
+			    (f-filename base-dir)))
+	     (link (-make-link short-name full-path))
+	     (heading (-make-heading link level)))
+	(if (or (null limit-level) (< level limit-level))
+	    (if (f-directory? base-dir)
+		(concat heading
+			(apply 'concat (mapcar
+					(lambda (d)  (-create-tree d (+ 1 level) limit-level))
+					(f-entries base-dir))))
+	      heading)
+	  heading))))
 
 ;;;###autoload
 (defun dump (arg dirname)
   "Dump the file system tree rooted at DIRNAME as an org tree.
 Each heading in the org-tree will be a link to the corresponding
-file or directory that can be opened using org-open-at-point. 
+file or directory that can be opened using org-open-at-point.
 
 Optional prefix argument can be used to limit the number of
 levels.
